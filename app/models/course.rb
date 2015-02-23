728c6724ececba83be9c4d4cb9b9aa9d92e6e97b
Cameron Webster <cameron.webster@gmail.com>
2015-02-23 10:04:22 -0700

    user can see course index page

---
+++ app/models/course.rb
@@ -0,0 +1,11 @@
+class Course < ActiveRecord::Base
+  # 
+  # def course_time
+  #   if Course.day?
+  #     @course_time == "Day"
+  #   else
+  #     @course_time == "Night"
+  #   end
+  # end
+
+end
