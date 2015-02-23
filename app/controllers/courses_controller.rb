c9cb14758794aeea3a2b5ae609de180dbff65ab9
Cameron Webster <cameron.webster@gmail.com>
2015-02-23 10:14:15 -0700

    user can add course and see course show page

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -4,4 +4,27 @@ class CoursesController < ApplicationController
     @courses = Course.all
   end
 
+  def new
+    @course = Course.new
+  end
+
+  def create
+    @course = Course.create(course_params)
+    if @course.save
+      redirect_to course_path(@course)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @course = Course.find(params[:id])
+  end
+
+
+
+  private
+  def course_params
+    params.require(:course).permit(:title, :day)
+  end
 end
